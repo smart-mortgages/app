0e6f758e864cda384c048d3c53e2d1ffe5c3a2ee
dzuroe <juraj.porubsky@vacuumlabs.com>
2025-06-19 20:12:13 +0200

    add backend mocks

--- backend/src/routes/customers.ts
+++ backend/src/routes/customers.ts
@@ -3,35 +3,40 @@ import {client} from "../web3client";
 import {customerABI, mortgageManagerABI, mortgageManagerAddress} from "../contracts";
 import {Address} from "viem";
 import {bigintReplacer} from "../utils/bigIntReplacer";
+import fs from "fs";
 
 const router = Router();
 
 router.get('/', async (req: Request, res: Response) => {
-    try {
-        const customers = await client.readContract({
-            address: mortgageManagerAddress as `0x${string}`,
-            abi: mortgageManagerABI,
-            functionName: 'getAllCustomers',
-            args: []
-        }) as Address[];
+    res.setHeader('Content-Type', 'application/json');
+    res.send(fs.readFileSync(__dirname + '/../../resources/mocks/customers.json', 'utf8'));
+    return;
 
-        const customerDataPromises = customers.map(async (customer) => {
-            const data = await client.readContract({
-                address: customer,
-                abi: customerABI,
-                functionName: 'getData',
-                args: []
-            });
-            return data;
-        });
-
-        const customersData = await Promise.all(customerDataPromises);
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(customersData, bigintReplacer));
-    } catch (error) {
-        console.error('Error fetching customers:', error);
-        res.status(500).json({error: 'Failed to fetch customers'});
-    }
+    // try {
+    //     const customers = await client.readContract({
+    //         address: mortgageManagerAddress as `0x${string}`,
+    //         abi: mortgageManagerABI,
+    //         functionName: 'getAllCustomers',
+    //         args: []
+    //     }) as Address[];
+    //
+    //     const customerDataPromises = customers.map(async (customer) => {
+    //         const data = await client.readContract({
+    //             address: customer,
+    //             abi: customerABI,
+    //             functionName: 'getData',
+    //             args: []
+    //         });
+    //         return data;
+    //     });
+    //
+    //     const customersData = await Promise.all(customerDataPromises);
+    //     res.setHeader('Content-Type', 'application/json');
+    //     res.send(JSON.stringify(customersData, bigintReplacer));
+    // } catch (error) {
+    //     console.error('Error fetching customers:', error);
+    //     res.status(500).json({error: 'Failed to fetch customers'});
+    // }
 });
 
 export default router;
--- backend/src/routes/smartMortgages.ts
+++ backend/src/routes/smartMortgages.ts
@@ -3,58 +3,63 @@ import {client} from "../web3client";
 import {mortgageManagerABI, mortgageManagerAddress, smartMortgageABI} from "../contracts";
 import {Address} from "viem";
 import {bigintReplacer} from "../utils/bigIntReplacer";
+import fs from "fs";
 
 const router = Router();
 
 router.get('/', async (req: Request, res: Response) => {
-    try {
-        const mortgages = await client.readContract({
-            address: mortgageManagerAddress as `0x${string}`,
-            abi: mortgageManagerABI,
-            functionName: 'getAllSmartMortgages',
-            args: []
-        }) as Address[];
+    res.setHeader('Content-Type', 'application/json');
+    res.send(fs.readFileSync(__dirname + '/../../resources/mocks/mortgages.json', 'utf8'));
+    return;
 
-        const mortgagePromises = mortgages.map(async (mortgage) => {
-            const data = await client.readContract({
-                address: mortgage,
-                abi: smartMortgageABI,
-                functionName: 'getData',
-                args: []
-            });
-            const rules = await client.readContract({
-                address: mortgage,
-                abi: smartMortgageABI,
-                functionName: 'getRules',
-                args: []
-            });
-            const personalIdNumber = await client.readContract({
-                address: mortgage,
-                abi: smartMortgageABI,
-                functionName: 'getCustomerPersonalIdNumber',
-                args: []
-            });
-            const interestRate = await client.readContract({
-                address: mortgage,
-                abi: smartMortgageABI,
-                functionName: 'getInterestRate',
-                args: []
-            });
-            return {
-                data: data,
-                rules: rules,
-                personalIdNumber: personalIdNumber,
-                interestRate: interestRate,
-            };
-        });
-
-        const mortgageResponse = await Promise.all(mortgagePromises);
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(mortgageResponse, bigintReplacer));
-    } catch (error) {
-        console.error('Error fetching customers:', error);
-        res.status(500).json({error: 'Failed to fetch customers'});
-    }
+    // try {
+    //     const mortgages = await client.readContract({
+    //         address: mortgageManagerAddress as `0x${string}`,
+    //         abi: mortgageManagerABI,
+    //         functionName: 'getAllSmartMortgages',
+    //         args: []
+    //     }) as Address[];
+    //
+    //     const mortgagePromises = mortgages.map(async (mortgage) => {
+    //         const data = await client.readContract({
+    //             address: mortgage,
+    //             abi: smartMortgageABI,
+    //             functionName: 'getData',
+    //             args: []
+    //         });
+    //         const rules = await client.readContract({
+    //             address: mortgage,
+    //             abi: smartMortgageABI,
+    //             functionName: 'getRules',
+    //             args: []
+    //         });
+    //         const personalIdNumber = await client.readContract({
+    //             address: mortgage,
+    //             abi: smartMortgageABI,
+    //             functionName: 'getCustomerPersonalIdNumber',
+    //             args: []
+    //         });
+    //         const interestRate = await client.readContract({
+    //             address: mortgage,
+    //             abi: smartMortgageABI,
+    //             functionName: 'getInterestRate',
+    //             args: []
+    //         });
+    //         return {
+    //             data: data,
+    //             rules: rules,
+    //             personalIdNumber: personalIdNumber,
+    //             interestRate: interestRate,
+    //         };
+    //     });
+    //
+    //     const mortgageResponse = await Promise.all(mortgagePromises);
+    //     res.setHeader('Content-Type', 'application/json');
+    //     res.send(JSON.stringify(mortgageResponse, bigintReplacer));
+    // } catch (error) {
+    //     console.error('Error fetching customers:', error);
+    //     res.status(500).json({error: 'Failed to fetch customers'});
+    // }
 });
 
 export default router;
